2615cae6fae76a541d0b53d9478b461c7f72732c
nastyaiva <148487824+nastyaiva@users.noreply.github.com>
2024-05-16 03:05:06 +0300

    tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,53 +2,45 @@
 #include <iostream>
 #include <sstream>
 #include "./../Command.cpp"
-// Include the header files for the classes you want to test
-#include "Command.hpp"
+#include <gtest/gtest.h>
+#include "CommandPattern.h"
 
-// Test fixture for Receiver class
-class ReceiverTest : public ::testing::Test {
-protected:
+// Test case to check if the receiver performs the action correctly
+TEST(CommandPatternTest, ReceiverAction) {
   Receiver receiver;
-};
-
-TEST_F(ReceiverTest, ActionTest) {
+  
   testing::internal::CaptureStdout();
   receiver.action();
   std::string output = testing::internal::GetCapturedStdout();
-  EXPECT_EQ(output, "Receiver: execute action\n");
+
+  ASSERT_EQ(output, "Receiver: execute action\n");
 }
 
-// Test fixture for ConcreteCommand class
-class ConcreteCommandTest : public ::testing::Test {
-protected:
-  Receiver* receiver = new Receiver();
-  Command* command = new ConcreteCommand(receiver);
-};
-
-TEST_F(ConcreteCommandTest, ExecuteTest) {
+// Test case to check if the concrete command executes correctly
+TEST(CommandPatternTest, CommandExecute) {
+  Receiver *receiver = new Receiver();
+  ConcreteCommand command(receiver);
+  
   testing::internal::CaptureStdout();
-  command->execute();
+  command.execute();
   std::string output = testing::internal::GetCapturedStdout();
-  EXPECT_EQ(output, "Receiver: execute action\n");
+
+  ASSERT_EQ(output, "Receiver: execute action\n");
+  delete receiver;
 }
 
-// Test fixture for Invoker class
-class InvokerTest : public ::testing::Test {
-protected:
-  Receiver* receiver = new Receiver();
-  Command* command = new ConcreteCommand(receiver);
+// Test case to check if the invoker confirms the command action
+TEST(CommandPatternTest, InvokerConfirm) {
+  Receiver *receiver = new Receiver();
+  ConcreteCommand command(receiver);
   Invoker invoker;
-};
-
-TEST_F(InvokerTest, ConfirmTest) {
-  invoker.set(command);
+  invoker.set(&command);
+  
   testing::internal::CaptureStdout();
   invoker.confirm();
   std::string output = testing::internal::GetCapturedStdout();
-  EXPECT_EQ(output, "Receiver: execute action\n");
+
+  ASSERT_EQ(output, "Receiver: execute action\n");
+  delete receiver;
 }
 
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
